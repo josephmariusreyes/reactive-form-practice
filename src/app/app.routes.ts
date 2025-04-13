51cf6a5bed3e5b7becf4f6d740c0198ff0b7f4ec
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-04-13 21:44:20 +0800

    Reactive form practice simple example 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ReactiveFormTest1Component } from '../reactive-form-test-1/reactive-form-test-1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'test1', pathMatch: 'full' },
+  { path: 'test1', component: ReactiveFormTest1Component }
+];
---
+++ src/reactive-form-test-1/reactive-form-test-1.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form-test-1',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './reactive-form-test-1.component.html',
+  styleUrl: './reactive-form-test-1.component.scss'
+})
+export class ReactiveFormTest1Component implements OnInit {
+
+  form!: FormGroup<IFriendsForm>;
+
+  constructor(private fb:FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    const staticData:DtoFriends[] = [
+      { name: 'Alice', age: 25 },
+      { name: 'Bob', age: 30 },
+      { name: 'Charlie', age: 22 }
+    ];
+    this.form = this.createFriendsForm(staticData);
+  }
+  
+  get friendsArray() {
+    return this.form.get('friends') as FormArray<FormGroup<FriendForm>>;
+  }
+
+  onSubmit(): void {
+    alert(JSON.stringify(this.form.getRawValue(), null, 2));
+  }
+
+  private createFriendsForm(data: { name: string; age: number }[]): FormGroup<IFriendsForm> {
+    const friendsFG = data.map(d => this.createFriendForm(d));
+    return this.fb.group<IFriendsForm>({
+      friends: this.fb.array<FormGroup<FriendForm>>(friendsFG)
+    });
+  }  
+  private createFriendForm(data:DtoFriends): FormGroup<FriendForm> {
+    return new FormGroup<FriendForm>({
+      name: new FormControl(data?.name ?? '', { nonNullable: true, validators: [Validators.required] }),
+      age: new FormControl(data?.age ?? 0, { nonNullable: true, validators: [Validators.min(0)] })
+    });
+  }
+  
+}
+
+//dto from API
+export interface DtoFriends {
+  name:string;
+  age:number;
+}
+
+//form interface
+export interface IFriendsForm {
+  friends: FormArray<FormGroup<FriendForm>>;
+}
+export interface FriendForm {
+  name: FormControl<string>;
+  age: FormControl<number>;
+}
