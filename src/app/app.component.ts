fb0540a833ad2e0b894791576ff753420b4915bb
Joseph Marius Reyes <joseph.marius.reyes@gmail.com>
2025-07-29 21:30:32 +0800

    form examples app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'reactive-form-practice';
+  showNavigation = true;
+
+  constructor(public router: Router) {
+    // Show navigation only on the root path
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavigation = event.url === '/' || event.url === '/reactiveform-test1';
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,51 @@ import { ReactiveFormBasicForm } from './reactive-form-basic-form/reactive-form-
 import { ReactiveFormBasicFormArray } from './reactive-form-basic-form-array/reactive-form-basic-form-array.component';
 import { ReactiveFormUsingDIfferentKindOfFields } from './reactive-form-using-diffrent-kind-of-fields/reactive-form-using-diffrent-kind-of-fields.component';
 import { RxjsPractice1Component } from './rxjs-practice1/rxjs-practice1.component';
+import { DynamicNestedFormComponent } from './dynamic-nested-form/dynamic-nested-form.component';
+import { CustomValidatorsFormComponent } from './custom-validators-form/custom-validators-form.component';
+import { AsyncValidatorsFormComponent } from './async-validators-form/async-validators-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'reactiveform-test1', pathMatch: 'full' },
   
-  //reactive form tests
-  { path: 'reactiveform-test1', component: ReactiveFormBasicForm },
-  { path: 'reactiveform-test2', component: ReactiveFormBasicFormArray },
-  { path: 'reactiveform-test3', component:ReactiveFormUsingDIfferentKindOfFields },
+  // Basic reactive form examples
+  { 
+    path: 'reactiveform-test1', 
+    component: ReactiveFormBasicForm,
+    title: 'Basic Form Array - Friends List'
+  },
+  { 
+    path: 'reactiveform-test2', 
+    component: ReactiveFormBasicFormArray,
+    title: 'Advanced Form Array - Friends Management'
+  },
+  { 
+    path: 'reactiveform-test3', 
+    component: ReactiveFormUsingDIfferentKindOfFields,
+    title: 'Different Field Types - User Profile'
+  },
 
-  //
-  { path: 'rxjs-test1', component:RxjsPractice1Component }
+  // Advanced reactive form examples
+  { 
+    path: 'reactiveform-test4', 
+    component: DynamicNestedFormComponent,
+    title: 'Dynamic Nested Forms - Company & Employees'
+  },
+  { 
+    path: 'reactiveform-test5', 
+    component: CustomValidatorsFormComponent,
+    title: 'Custom Validators - Registration Form'
+  },
+  { 
+    path: 'reactiveform-test6', 
+    component: AsyncValidatorsFormComponent,
+    title: 'Async Validators - Account Setup'
+  },
+
+  // RxJS practice
+  { 
+    path: 'rxjs-test1', 
+    component: RxjsPractice1Component,
+    title: 'RxJS Practice'
+  }
 ];
